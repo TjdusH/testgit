f5010f9f7fe3eaeb8a44f54dff005f058082b377
TjdusH <h.yeon030728@gmail.com>
2025-11-14 13:15:50 +0900

    Initial commit

---
+++ gemini-react-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello, Gemini!</h1>
+        <p>This is your first React application.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
